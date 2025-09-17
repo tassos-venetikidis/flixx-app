fb41c9cd1a6bccb717d3dae17ee7eecd6cc04e42
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2025-09-17 14:19:02 +0300

    Added the display popular movies functionality on the home page

--- js/script.js
+++ js/script.js
@@ -2,6 +2,62 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+// Fetch Data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_URL = "https://api.themoviedb.org/3/";
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MWM4ZDhiMDk4OGY2OGQzMTJkYWI5N2FhZTIyMDQyNiIsIm5iZiI6MTc1ODAxNDM5OC45MSwic3ViIjoiNjhjOTJiYmVjY2ZiMzM0OWE1ODA0ZjYyIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.8WVq27euFIHWkIWjsd-lVk0aGAs7yAj1hVEqJcxDsW4",
+    },
+  };
+  const response = await fetch(`${API_URL}${endpoint}?language=en-US`, options);
+  const data = response.json();
+  return data;
+}
+
+// Display Popular Movies Function
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("movie/popular");
+  const popularMoviesGrid = document.getElementById("popular-movies");
+  for (const result of results) {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    const imageLink = document.createElement("a");
+    imageLink.setAttribute("href", `movie-details.html?id=${result.id}`);
+    const image = document.createElement("img");
+    if (result.poster_path) {
+      image.setAttribute(
+        "src",
+        `http://image.tmdb.org/t/p/w500${result.poster_path}`
+      );
+    } else {
+      image.setAttribute("src", "../images/no-image.jpg");
+    }
+    image.setAttribute("alt", `${result.title}`);
+    image.classList.add("card-img-top");
+    imageLink.append(image);
+    cardDiv.append(imageLink);
+    const cardBodyDiv = document.createElement("div");
+    cardBodyDiv.classList.add("card-body");
+    const movieTitleH5 = document.createElement("h5");
+    movieTitleH5.classList.add("card-title");
+    movieTitleH5.textContent = result.title;
+    cardBodyDiv.append(movieTitleH5);
+    const cardTextP = document.createElement("p");
+    cardTextP.classList.add("card-text");
+    const smallElement = document.createElement("small");
+    smallElement.classList.add("text-muted");
+    smallElement.textContent = `Release ${result.release_date}`;
+    cardTextP.append(smallElement);
+    cardBodyDiv.append(cardTextP);
+    cardDiv.append(cardBodyDiv);
+    popularMoviesGrid.append(cardDiv);
+  }
+}
+
 // Highlight active link
 function highlightActiveLink() {
   const headerLinks = document.querySelectorAll(".main-header .nav-link");
@@ -18,6 +74,7 @@ function init() {
     case "/":
     case "/index.html":
       console.log("HOME");
+      displayPopularMovies();
       break;
     case "/shows.html":
       console.log("SHOWS");
